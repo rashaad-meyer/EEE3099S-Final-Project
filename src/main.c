f4c1c06e16c0673b1704493f660f424645e73a91
Shafiq Meyer <01432300@wf.uct.ac.za>
2019-09-23 11:58:46 +0200

    Testing commits on laptop

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ******************************************************************************
 */
-
+//testing github
 /* Includes */
 #include "stm32f0xx.h"
 
